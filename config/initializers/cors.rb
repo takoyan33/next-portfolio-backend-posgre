d5ec344ff79f4849f17122a54fce259fa97d64d9
abeshumpei <harrier2070@gmail.com>
2025-10-31 22:57:33 +0900

    add: 認証apiの追加

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000', 'localhost:3001','https://to-you-design.vercel.app/'
+    origins 'localhost:3000','localhost:3001','https://to-you-design.vercel.app/'
 
     resource "*",
       headers: :any,
